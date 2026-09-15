ff8165af84be8fa2b7feb5fba5aa6f791fe7caf3
Lauttd <lautaroayala123090@gmail.com>
2026-09-15 16:14:17 -0300

    first commit

---
+++ src/repositories/employee.repository.ts
@@ -0,0 +1,19 @@
+import { Employee } from '../models/employee.model'
+
+export class EmployeeRepository {
+   async guardarEmpleado ( datosEmpleados: any ) {
+        const employee = await Employee.create( datosEmpleados );
+        return employee 
+   } 
+
+   async buscarTodosEmpleados( datosEmpleados: any ) {
+     const employees = await Employee.find().sort({ createdAt: -1 });
+        return employees
+   }
+
+   async buscarEmpeladoId( id: string,) {
+     const employee = await Employee.findById( id );
+         return employee
+    }
+}
+
---
+++ src/services/employee.service.ts
@@ -0,0 +1,30 @@
+import { EmployeeRepository } from '../repositories/employee.repository';
+
+export interface DatosCrearEmpleado {
+    name: string
+    position: string
+    baseSalary: number
+    yearsOfService: number
+}
+
+export class EmployeeService {
+    constructor( private repository: EmployeeRepository ) {}
+    
+    async crearEmpleadoNuevo( datosEmpleados: DatosCrearEmpleado ) {
+        const bonus = datosEmpleados.baseSalary * 0.02 * datosEmpleados.yearsOfService
+
+        const finalSalary = datosEmpleados.baseSalary + bonus;
+
+        const empleadoCompleto = {
+            name: datosEmpleados.name,
+            position: datosEmpleados.position,
+            baseSalary: datosEmpleados.baseSalary,
+            yearsOfService: datosEmpleados.yearsOfService,
+            finalSalary: finalSalary
+        }
+
+        const resultado = await this.repository.guardarEmpleado(empleadoCompleto)
+        
+        return resultado;
+    }
+}
